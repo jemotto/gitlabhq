6512ee904f1faf7680a9441f14c749a2ef5c8074
Jasiek Otto <jan.otto@cern.ch>
2013-03-26 15:49:46 +0100

    change routing and override after_sign_out_path_for to delete gitlab session of user when siging out from sso

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -135,4 +135,10 @@ class ApplicationController < ActionController::Base
   def dev_tools
     Rack::MiniProfiler.authorize_request
   end
+
+  private
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    "https://login.cern.ch/adfs/ls/?wa=wsignout1.0"
+  end
 end
